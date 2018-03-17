7422eaa3fbd84b4bf347bbd73b85484f133313e5
Henry Golding <hegold@users.noreply.github.com>
2018-03-17 12:00:57 -0700

    Start in starting state and expose question and answer words

--- restaurant/src/StateMachine.js
+++ restaurant/src/StateMachine.js
@@ -3,9 +3,18 @@ import Phaser from 'phaser'
 export default class {
     constructor(stateData) {
         this.stateData = stateData;
+        this.currentState = this.stateData.States[this.stateData.StartAt];
     }   
 
     printDebugInfo() {
         console.log(JSON.stringify(this.stateData));
+    }
+
+    getQuestion() {
+        return this.currentState.Question;
+    }
+
+    getAnswerWords() {
+        return this.currentState.AnswerWords;
     }
 }
