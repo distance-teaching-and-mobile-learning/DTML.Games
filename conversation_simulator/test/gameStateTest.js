c78a530428d027316515a23ececeb69fd4abd93f
Marshall <marshcannon@gmail.com>
2019-01-29 16:14:59 -0700

    Attempting to use puppeteer to run unit tests on a headless browser

--- conversation_simulator/test/gameStateTest.js
+++ conversation_simulator/test/gameStateTest.js
@@ -1,15 +1,21 @@
 // var assert = require('assert')
 // var sinon = require('sinon')
-// var mockBrowser = require('mock-browser').mocks.MockBrowser
-// global.window = new mockBrowser()
-// global.document = window.getDocument()
-// global.PIXI = require('phaser-ce/build/custom/pixi.js')
-// global.p2 = require('phaser-ce/build/custom/p2.js')
-// global.Phaser = require('phaser-ce/build/custom/phaser-split.js')
-// var GameState = require('../src/states/game.js')
+// var puppeteer = require('puppeteer')
+// var GameState = null
 
 // describe('Game State', function () {
 //   var Game
+  
+//   before (async function () {
+//     console.log('starting to load browser')
+//     global.browser = await puppeteer.launch()
+//     global.window = await global.browser.newPage()
+//     global.PIXI = require('phaser-ce/build/custom/pixi.js')
+//     global.p2 = require('phaser-ce/build/custom/p2.js')
+//     global.Phaser = require('phaser-ce/build/custom/phaser-split.js')
+//     GameState = require('../src/states/game.js')
+//     console.log('browser loaded')
+//   })
   
 //   beforeEach(function() {
 //     Game = new GameState.default()
@@ -19,12 +25,12 @@
 //     sinon.restore()
 //   })
 
+//   after (function () {
+//     global.browser.close()
+//   })
+
 //   describe('loadSpriter', function () {
-//     global.game = {
-//       cache: {
-//         getXML: sinon.fake.returns()
-//       }
-//     }
+//     global.game = new Phaser.Game()
 //     it('should load a new Spriter object given the key \'leftCharacter\'', function () {
 //       assert.ok(Game.loadSpriter('leftCharacter'))
 //     })
