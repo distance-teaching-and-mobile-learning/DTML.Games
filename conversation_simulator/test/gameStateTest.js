996ceb3bfe028e7e3349e6d9306f0c4a07903e2a
Marshall <marshcannon@gmail.com>
2019-02-02 03:11:04 -0700

    More work on mocking browser environment

--- conversation_simulator/test/gameStateTest.js
+++ conversation_simulator/test/gameStateTest.js
@@ -1,46 +1,44 @@
-// var assert = require('assert')
-// var sinon = require('sinon')
-// var puppeteer = require('puppeteer')
-// var path = require('path')
-// var GameState = null
+var assert = require('assert')
+var sinon = require('sinon')
+var puppeteer = require('puppeteer')
+var path = require('path')
+var GameState = null
 
-// describe('Game State', function () {
-//   var Game
+describe('Game State', function () {
+  var Game
   
-//   before (async function () {
-//     global.browser = await puppeteer.launch()
-//     global.window = await global.browser.newPage()
-//     await window.goto('file://' + path.join(__dirname + '/mockData/index.html'))
-//     let globalVars = await window.evaluate(function () {
-//       return {
-//         PIXI: require('phaser-ce/build/custom/pixi.js'),
-//         p2: require('phaser-ce/build/custom/p2.js'),
-//         Phaser: require('phaser-ce/build/custom/phaser-split.js'),
-//         GameState: require('../src/states/game.js'),
-//       }
-//     })
-//     global.PIXI = globalVars.PIXI
-//     global.p2 = globalVars.p2
-//     global.Phaser = globalVars.Phaser
-//     GameState = globalVars.GameState
-//     global.game = new Phaser.Game()
-//   })
+  before (async function () {
+    global.browser = await puppeteer.launch()
+    global.window = await global.browser.newPage()
+    await window.goto('file://' + path.join(__dirname + '/mockData/build/index.html'))
+    // global.document = await window.evaluate(function () {
+    //   return document
+    // })
+    // global.PIXI = require('phaser-ce/build/custom/pixi.js')
+    // global.p2 = require('phaser-ce/build/custom/p2.js')
+    // global.Phaser = require('phaser-ce/build/custom/phaser-split.js')
+    // GameState = require('../src/states/game.js')
+    // global.game = new Phaser.Game()
+  })
   
-//   beforeEach(function() {
-//     Game = new GameState.default()
-//   })
+  beforeEach(function() {
+    // Game = new GameState.default()
+  })
 
-//   afterEach(function() {
-//     sinon.restore()
-//   })
+  afterEach(function() {
+    sinon.restore()
+  })
 
-//   after (function () {
-//     global.browser.close()
-//   })
+  after (function () {
+    global.browser.close()
+  })
 
-//   describe('loadSpriter', function () {
-//     it('should load a new Spriter object given the key \'leftCharacter\'', function () {
-//       assert.ok(Game.loadSpriter('leftCharacter'))
-//     })
-//   })
-// })
+  describe('loadSpriter', function () {
+    it('should load a new Spriter object given the key \'leftCharacter\'', async function (done) {
+      await window.evaluate(function() {
+        let newSpriter = Game.loadSpriter('leftCharacter')
+        done(false)
+      })
+    })
+  })
+})
