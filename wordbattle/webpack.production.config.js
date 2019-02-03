edf914fa1f25075e24187939cb23e132138317a0
Aleksey <aleksin@dtml.org>
2019-02-02 22:41:45 -0800

    Updating build to new webpack

--- wordbattle/webpack.production.config.js
+++ wordbattle/webpack.production.config.js
@@ -1,3 +1,4 @@
+process.traceDeprecation = true
 var path = require('path')
 var webpack = require('webpack')
 var CleanWebpackPlugin = require('clean-webpack-plugin')
@@ -23,22 +24,16 @@ module.exports = {
     vendor: ['pixi', 'p2', 'phaser', 'webfontloader']
 
   },
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, 'build'),
     publicPath: './',
-    filename: 'js/bundle.js'
+    filename: 'js/[name].js'
   },
   plugins: [
     definePlugin,
     new CleanWebpackPlugin(['build']),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
-    new webpack.optimize.UglifyJsPlugin({
-      drop_console: true,
-      minimize: true,
-      output: {
-        comments: false
-      }
-    }),
     new HtmlWebpackPlugin({
       filename: 'index.html', // path.resolve(__dirname, 'build', 'index.html'),
       template: './src/index.html',
