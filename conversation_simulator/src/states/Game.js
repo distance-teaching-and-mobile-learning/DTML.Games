b4f309c99c14e1f45bee616c618fd821e08c529c
Marshall <marshcannon@gmail.com>
2019-02-15 12:45:45 -0700

    Removed a bunch of unused images

--- conversation_simulator/src/states/Game.js
+++ conversation_simulator/src/states/Game.js
@@ -481,7 +481,6 @@ export default class extends Phaser.State {
           }
 
           if (this.bgnya !== '') {
-            // this.load.image('bgn', 'assets/images/res/backgrounds/'+this.bgnya);
             this.bg.bgs[0].loadTexture(this.bgnya)
           }
         }
