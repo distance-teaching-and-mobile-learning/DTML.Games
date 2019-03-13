69e4ae0331793732b67c8cacffa281bfc9d9166d
Aleksey <aleksin@dtml.org>
2019-03-12 20:53:38 -0700

    small bug fixing

--- conversation_simulator/src/StateMachine.js
+++ conversation_simulator/src/StateMachine.js
@@ -25,6 +25,10 @@ export default class {
   getScore () {
     return this.score
   }
+  
+ getCurrentStateName () {
+    return  this.currentStateName
+  }
 
   getOnEnterLeft () {
     if (this.currentState.OnStateEnter != null) {
