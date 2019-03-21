0b4f3a31e7105d240364d35c7532e1b59cb7e9d4
Aleksey <aleksin@dtml.org>
2019-03-21 07:21:21 -0700

    removed debugger

--- conversation_simulator/src/states/Game.js
+++ conversation_simulator/src/states/Game.js
@@ -478,7 +478,7 @@ export default class extends Phaser.State {
 
   nextQuestion (text, submitResult) {
     if (text === '') {
-	  dtml.recordGameEnd("conversation_"+this.phaserJSON.Setup.Name, this.scoreText.text, "End");
+      dtml.recordGameEvent("conversation_"+this.phaserJSON.Setup.Name, "emptyText", this.stateMachine.getCurrentStateName());
       this.state.start('GameOver', true, false, this.scoreText.text)	  
     }
 
@@ -488,8 +488,7 @@ export default class extends Phaser.State {
         this.patienceBars[this.patienceRemaining - 1].kill()
         this.patienceRemaining -= 1
       } else {
-		  debugger;
-		dtml.recordGameEnd("conversation_"+this.phaserJSON.Setup.Name, this.scoreText.text, this.stateMachine.getCurrentStateName());
+	dtml.recordGameEnd("conversation_"+this.phaserJSON.Setup.Name, this.scoreText.text, his.stateMachine.getCurrentStateName());
         this.state.start('GameOver', true, false, this.scoreText.text)
         return
       }
@@ -565,7 +564,6 @@ export default class extends Phaser.State {
               0
             )
             .onComplete.add(() => {
-              // this.rightCharacter.scale.x *= -1;
               this.rightCharacter.setAnimationSpeedPercent(100)
               this.rightCharacter.playAnimationByName('_IDLE')
             })
