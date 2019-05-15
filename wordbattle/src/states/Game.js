44a8affe024bf7165503353884325cd8d0d89474
Marshall <marshcannon@gmail.com>
2019-05-14 17:30:27 -0700

    Removed testing feature

--- wordbattle/src/states/Game.js
+++ wordbattle/src/states/Game.js
@@ -375,7 +375,6 @@ export default class extends Phaser.State {
             .then(res => res.json())
             .then(data => {
                 this.errorText.hide();
-                data.isCorrect = true
                 if (data.isCorrect)
                     this.castSpell(data.complexity);
                     if (this.mode === 'challenge') {
